3670161190f32151e1ae2608575b7a306ca6fc7a
Gilberth Arce <gah.chubby@gmail.com>
2012-09-10 08:56:53 -0600

    Commit

---
+++ CompileDisplayLayer.bat
@@ -0,0 +1,3 @@
+"C:/Program Files/Java/jdk1.6.0_25/bin/javac" -cp "universidad/WEB-INF/classes";"universidad/WEB-INF/lib/*" -d universidad/WEB-INF/classes src/display/PageController.java src/display/ActualizarProfesor.java src/display/DetalleProfesor.java src/display/ListaProfesores.java src/display/ListaGrupos.java src/display/ProfesorAssembler.java src/display/ProfesorDTO.java src/display/GrupoAssembler.java src/display/GrupoDTO.java src/display/AgregarProfesor.java src/display/EliminarProfesor.java src/display/EliminarGrupo.java src/display/NuevoGrupo.java src/display/AgregarGrupo.java src/display/DetalleGrupo.java src/display/ActualizarGrupo.java
+
+pause
---
+++ CompileDomainLayer.bat
@@ -0,0 +1,3 @@
+"C:/Program Files/Java/jdk1.6.0_25/bin/javac" -d universidad/WEB-INF/classes src/domain/Profesor.java src/domain/ProfesorFactory.java src/domain/ProfesorRepository.java src/domain/Grupo.java src/domain/GrupoFactory.java src/domain/GrupoRepository.java
+
+pause
